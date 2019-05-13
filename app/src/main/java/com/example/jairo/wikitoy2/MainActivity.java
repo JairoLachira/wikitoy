16170105fbcd493feff38d076172a7703e0d26b8
Jairo LP <jairo.5799@hotmail.com>
2019-05-13 01:48:54 -0500

    commit antes de ir a dormid, fragments funcionando, me quede en los videos

--- app/src/main/java/com/example/jairo/wikitoy2/MainActivity.java
+++ app/src/main/java/com/example/jairo/wikitoy2/MainActivity.java
@@ -1,25 +1,72 @@
 package com.example.jairo.wikitoy2;
 
+import android.support.v4.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+    MainFragment.OnFragmentInteractionListener, TutorialesFragment.OnFragmentInteractionListener,
+        DeveloperFragment.OnFragmentInteractionListener
+{
 
     private TextView tvTituloNombre;
-
+    MenuItem inicioItem, tutotialesItem;
+    private BottomNavigationView menuItem;
+    FragmentTransaction transition = null;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SharedPreferences prefs = getSharedPreferences("Share", Context.MODE_PRIVATE );
-        showToolbar("¿Qué deseas hacer " + prefs.getString("nombreuser","") + "?", false);
+
+        menuItem=(BottomNavigationView)findViewById(R.id.menuItem);
+
+        MainFragment inicioFragment = new MainFragment();
+        transition =  getSupportFragmentManager().beginTransaction();
+        transition.replace(R.id.container, inicioFragment);
+        transition.commit();
+
+        menuItem.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.inicioItem:
+                        MainFragment inicioFragment = new MainFragment();
+                        transition =  getSupportFragmentManager().beginTransaction();
+                        transition.replace(R.id.container, inicioFragment);
+                        transition.commit();
+                        break;
+                    case R.id.tutorialesItem:
+                        TutorialesFragment tutorialesFragment =new TutorialesFragment();
+                        transition =  getSupportFragmentManager().beginTransaction();
+                        transition.replace(R.id.container, tutorialesFragment);
+                        transition.commit();
+                        break;
+                    case R.id.desarrolladorItem:
+                        DeveloperFragment developerFragment =new DeveloperFragment();
+                        transition =  getSupportFragmentManager().beginTransaction();
+                        transition.replace(R.id.container, developerFragment);
+                        transition.commit();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        showToolbar("Hola " + prefs.getString("nombreuser",""), false);
     }
 
     public void showToolbar(String title, boolean upButton)
@@ -40,5 +87,10 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent i=new Intent(this, EjerciciosActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
